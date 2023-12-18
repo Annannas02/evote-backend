97dbc744b2c842d71f47667a62b9700e28b65e57
g4mrpr0n <georgeana.globa@gmail.com>
2023-12-18 22:03:38 +0200

    small tweaks to election GETs

--- evoteapp/electionchoice/models.py
+++ evoteapp/electionchoice/models.py
@@ -3,4 +3,6 @@ from elections.models import Elections
 
 class ElectionChoice(models.Model):
     election_id = models.ForeignKey(Elections, on_delete=models.CASCADE)
-    choice_description = models.CharField(max_length=2000)
+    img = models.ImageField(upload_to='choice_images/', null=True, blank=True)
+    description = models.CharField(max_length=2000)
+    
--- evoteapp/electionchoice/serializers.py
+++ evoteapp/electionchoice/serializers.py
@@ -6,3 +6,9 @@ class ElectionChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ElectionChoice
         fields = '__all__'
+    
+    def to_representation(self, instance):
+        # Exclude 'election_id' only from the output representation
+        data = super().to_representation(instance)
+        data.pop('election_id', None)
+        return data
